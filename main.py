0ee4e05c810d0d25c07aeb6d34e7dead567196db
esolarmo <eero.solarmo@visma.com>
2025-04-08 14:43:28 +0300

    beta version

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from stats import get_num_words
+from stats import get_num_chars
+from stats import sorted_counts
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         contents = f.read()
     return contents
 
-def main():
-    text = get_book_text("books/frankenstein.txt")
-    get_num_words(text)
-    
+def main(path_to_book):
+    text = get_book_text(path_to_book)
+    wordcount = get_num_words(text)
 
-main()
+    char_counts = get_num_chars(text)
+    sorted = sorted_counts(char_counts)
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {wordcount} total words")
+    print("--------- Character Count -------")
+    for s in sorted:
+        if s["char"].isalpha() == True:
+            print(f"{s["char"]}: {s["num"]}")
+    print("============= END ===============")
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    main(sys.argv[1])
--- stats.py
+++ stats.py
@@ -5,4 +5,42 @@ def get_num_words(content):
     for word in words:
         word_count += 1
     
-    print(f"{word_count} words found in the document")
+    # print(f"{word_count} words found in the document")
+    return(word_count)
+
+def get_num_chars(content):
+    char_counts = {}
+
+    for char in content:
+        char = char.lower()
+        if char not in char_counts:
+            char_counts[char] = 1
+        else:
+            char_counts[char] += 1
+
+    #print(char_counts)
+    return(char_counts)
+
+def sort_on(dict):
+    return dict["num"]
+
+def sorted_counts(char_counts):
+    sorted_list = []
+    
+    for char in char_counts:
+        dict = {}        
+        dict["char"] = char
+        dict["num"] = char_counts[char]
+        sorted_list.append(dict)
+        
+
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
+    
+
+    
+
+
+
+
